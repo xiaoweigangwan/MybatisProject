4f954784f7c6834271bf83fec56195de177a4739
xiaoweigangwan <1139118278@qq.com>
2017-10-11 15:46:28 +0800

    Update Run.java

--- Run.java
+++ Run.java
@@ -2,6 +2,6 @@ public class Run{
 public static void main(String[] args){
   SqlSessionFactory sqlSessionFactory = ;
   SqlSession sqlSession = sqlSessionFactory.openSession();
-  List<map> listUserinfo = sqlSession.selectList("");
+  List<map> listUserinfo = sqlSession.selectList("getAllUserinfo");
 }
 }
