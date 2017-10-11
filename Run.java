349ccd7d6036c02a286f6d8f539e6a60191f5118
xiaoweigangwan <1139118278@qq.com>
2017-10-11 15:57:11 +0800

    Update Run.java

--- Run.java
+++ Run.java
@@ -1,7 +1,21 @@
 public class Run{
 public static void main(String[] args){
-  SqlSessionFactory sqlSessionFactory = ;
+  SqlSessionFactory sqlSessionFactory = MyBatisUtil.getSqlSessionFactory();
   SqlSession sqlSession = sqlSessionFactory.openSession();
-  List<map> listUserinfo = sqlSession.selectList("getAllUserinfo");
+  List<Map> listUserinfo = sqlSession.selectList("getAllUserinfo");
+  for(int i=0;i<listUserinfo.size();i++)
+  {
+    Map map = listUserinfo.get(i);
+    System.out.println(map.get("ID")+" "+map.get("USERNAME"));
+  }
+  HasMap mapParam = new HasMap();
+  mapParam.put("id",5);
+  mapParam.put("ordersql","id desc");
+  List<Map> listMap = sqlSession.selectList("",mapParam);
+  for(int i=0;i<listMap.size();i++)
+  {
+    Map map = listMap.get(i);
+    System.out.println(map.get("ID")+" "+map.get("USRENAME"));
+  }
 }
 }
