c8644ae2a3d75d33fd010fae227a147d423984dd
xiaoweigangwan <1139118278@qq.com>
2017-10-11 13:43:29 +0800

    Create  UserInfo.java

---
+++ UserInfo.java
@@ -0,0 +1,7 @@
+public class UserInfo implements Serializable{
+    private Long id ;
+    private String userName;
+    private String password;
+    private Long age;
+    private Date insertDate;
+}
